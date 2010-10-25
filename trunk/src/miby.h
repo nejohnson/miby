50f389874a5267bc0f36ca77f7731b40f1a8b866
neil.johnson71 <neil.johnson71@5e8b938d-71b4-6f64-bc76-cdc99ae2ff1e>
2010-10-25 12:42:38 +0000

    Tabs to spaces.

--- trunk/src/miby.h
+++ trunk/src/miby.h
@@ -28,7 +28,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * ************************************************************************* */
- 
+
 #ifndef _MIBY_H
 #define _MIBY_H
 
@@ -45,11 +45,11 @@ typedef struct miby_s *miby_this_t;
 
 
 typedef struct miby_s {
-    unsigned char statusbyte; 
+    unsigned char statusbyte;
     unsigned char msg_chan;
     unsigned char sysexstate;
     unsigned char basic_channel;
-    unsigned char top_channel;	  
+    unsigned char top_channel;	
     int msglen;
     int ptr;
     void (*handler)(miby_this_t);
@@ -57,21 +57,21 @@ typedef struct miby_s {
     unsigned char buf[MIBY_SYSEX_BUF_LEN];
 } miby_t;
 
-#define MIBY_SYSEX_IDLE			( 0 )
-#define MIBY_SYSEX_START     		( 1 )
-#define MIBY_SYSEX_MID       		( 2 )
-#define MIBY_SYSEX_END       		( 3 )
-#define MIBY_SYSEX_ABORT     		( 4 )
+#define MIBY_SYSEX_IDLE             ( 0 )
+#define MIBY_SYSEX_START            ( 1 )
+#define MIBY_SYSEX_MID              ( 2 )
+#define MIBY_SYSEX_END              ( 3 )
+#define MIBY_SYSEX_ABORT            ( 4 )
 
 
-#define MIBY_STATUSBYTE(x)		((x)->statusbyte)
-#define MIBY_ARG0(x)			((x)->buf[0])
-#define MIBY_ARG1(x)			((x)->buf[1])
-#define MIBY_CHAN(x)			((x)->msg_chan)
-#define MIBY_SYSEX_LEN(x)		((x)->ptr)
-#define MIBY_SYSEX_BUF(x,y)		((x)->buf[(y)])
-#define MIBY_SYSEX_STATE(x)		((x)->sysexstate)
-#define MIBY_SYSEX_DONE_OK(x)		((x)->ptr=0)
+#define MIBY_STATUSBYTE(x)          ((x)->statusbyte)
+#define MIBY_ARG0(x)                ((x)->buf[0])
+#define MIBY_ARG1(x)                ((x)->buf[1])
+#define MIBY_CHAN(x)                ((x)->msg_chan)
+#define MIBY_SYSEX_LEN(x)           ((x)->ptr)
+#define MIBY_SYSEX_BUF(x,y)         ((x)->buf[(y)])
+#define MIBY_SYSEX_STATE(x)         ((x)->sysexstate)
+#define MIBY_SYSEX_DONE_OK(x)       ((x)->ptr=0)
 
 
 
--- trunk/src/miby_config.h
+++ trunk/src/miby_config.h
@@ -39,88 +39,88 @@
 
 /** Length of SysEx Receive Buffer **/
 #ifndef MIBY_SYSEX_BUF_LEN
-#define MIBY_SYSEX_BUF_LEN   		( 128 )
+#define MIBY_SYSEX_BUF_LEN          ( 128 )
 #endif
 
 /** Provide default values for handler names **/
 
 /** - System RealTime **/
 #ifndef MIBY_HND_RT_CLOCK
-#define MIBY_HND_RT_CLOCK		( NULL )
+#define MIBY_HND_RT_CLOCK           ( NULL )
 #endif
 
 #ifndef MIBY_HND_RT_START
-#define MIBY_HND_RT_START		( NULL )
+#define MIBY_HND_RT_START           ( NULL )
 #endif
 
 #ifndef MIBY_HND_RT_CONTINUE
-#define MIBY_HND_RT_CONTINUE		( NULL )
+#define MIBY_HND_RT_CONTINUE        ( NULL )
 #endif
 
 #ifndef MIBY_HND_RT_STOP
-#define MIBY_HND_RT_STOP		( NULL )
+#define MIBY_HND_RT_STOP            ( NULL )
 #endif
 
 #ifndef MIBY_HND_RT_ACT_SENSE
-#define MIBY_HND_RT_ACT_SENSE		( NULL )
+#define MIBY_HND_RT_ACT_SENSE       ( NULL )
 #endif
 
 #ifndef MIBY_HND_RT_SYS_RESET
-#define MIBY_HND_RT_SYS_RESET		( NULL )
+#define MIBY_HND_RT_SYS_RESET       ( NULL )
 #endif
 
 /** - System Common **/
 
 #ifndef MIBY_HND_SYS_MTC
-#define MIBY_HND_SYS_MTC		( NULL )
+#define MIBY_HND_SYS_MTC            ( NULL )
 #endif
 
 #ifndef MIBY_HND_SYS_SONGPOS
-#define MIBY_HND_SYS_SONGPOS		( NULL )
+#define MIBY_HND_SYS_SONGPOS        ( NULL )
 #endif
 
 #ifndef MIBY_HND_SYS_SONGSEL
-#define MIBY_HND_SYS_SONGSEL		( NULL )
+#define MIBY_HND_SYS_SONGSEL        ( NULL )
 #endif
 
 #ifndef MIBY_HND_SYS_TUNEREQ
-#define MIBY_HND_SYS_TUNEREQ		( NULL )
+#define MIBY_HND_SYS_TUNEREQ        ( NULL )
 #endif
 
 /** - System Exclusive **/
 
 #ifndef MIBY_HND_SYS_EX
-#define MIBY_HND_SYS_EX			( NULL )
+#define MIBY_HND_SYS_EX             ( NULL )
 #endif
 
 /** - Channel Voice **/
 
 #ifndef MIBY_HND_NOTE_ON
-#define MIBY_HND_NOTE_ON		( NULL )
+#define MIBY_HND_NOTE_ON            ( NULL )
 #endif
 
 #ifndef MIBY_HND_NOTE_OFF
-#define MIBY_HND_NOTE_OFF		( NULL )
+#define MIBY_HND_NOTE_OFF           ( NULL )
 #endif
 
 #ifndef MIBY_HND_POLY_AT
-#define MIBY_HND_POLY_AT		( NULL )
+#define MIBY_HND_POLY_AT            ( NULL )
 #endif
 
 #ifndef MIBY_HND_CTRL_CHG
-#define MIBY_HND_CTRL_CHG		( NULL )
+#define MIBY_HND_CTRL_CHG           ( NULL )
 #endif
 
 #ifndef MIBY_HND_PROG_CHG
-#define MIBY_HND_PROG_CHG		( NULL )
+#define MIBY_HND_PROG_CHG           ( NULL )
 #endif
 
 #ifndef MIBY_HND_CHAN_AT
-#define MIBY_HND_CHAN_AT		( NULL )
+#define MIBY_HND_CHAN_AT            ( NULL )
 #endif
 
 #ifndef MIBY_HND_PITCHBEND
-#define MIBY_HND_PITCHBEND		( NULL )
+#define MIBY_HND_PITCHBEND          ( NULL )
 #endif
 
 
