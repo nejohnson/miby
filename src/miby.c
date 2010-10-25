e42e3373018db7db462cdfccde2c8a1fb633a303
neil.johnson71 <neil.johnson71@5e8b938d-71b4-6f64-bc76-cdc99ae2ff1e>
2010-10-25 12:42:38 +0000

    Tabs to spaces.

--- src/miby.c
+++ src/miby.c
@@ -40,13 +40,13 @@
 
 
 
-#define BYTE_IS_DATA(b)	 		((b)<0x80)
-#define BYTE_IS_SYS(b)			((b)>=0xF0)
-#define BYTE_IS_CHAN(b)			((b)<0xF0)
-#define BYTE_IS_SYSRT(b) 		((b)>=0xF8)
+#define BYTE_IS_DATA(b)         ((b)<0x80)
+#define BYTE_IS_SYS(b)          ((b)>=0xF0)
+#define BYTE_IS_CHAN(b)         ((b)<0xF0)
+#define BYTE_IS_SYSRT(b)        ((b)>=0xF8)
 
-#define MASK_CHAN			(0x0F)
-#define MASK_CMD			(0xF0)
+#define MASK_CHAN               (0x0F)
+#define MASK_CMD                (0xF0)
 
 /** Define all the Status Bytes **/
 #define STATUS_CH_NOTE_OFF		(0x80)
@@ -63,7 +63,7 @@
 #define STATUS_SONG_POS			(0xF2)
 #define STATUS_SONG_SEL			(0xF3)
 #define STATUS_TUNE_REQ			(0xF6)
-#define STATUS_EOX			(0xF7)
+#define STATUS_EOX              (0xF7)
 
 #define STATUS_RT_TIMING		(0xF8)
 #define STATUS_RT_START			(0xFA)
@@ -79,7 +79,7 @@
 
 /*****************************************************************************/
 /**
-	Genreal MIDI Message Handler Table
+    Genreal MIDI Message Handler Table
 **/
 
 typedef void (*handler_t)(miby_t *);
@@ -113,17 +113,17 @@ static const msghandler_t msg_table[16] = {
 
 /*****************************************************************************/
 /**
-	Realtime System Exclusive Handler Table
+    Realtime System Exclusive Handler Table
 **/
 typedef void (*rt_handler_t)(miby_t *);
 
 static const rt_handler_t rt_handler_table[8] = {
     MIBY_HND_RT_CLOCK,
-    NULL,			/* reserved */
+    NULL, /* reserved */
     MIBY_HND_RT_START,
     MIBY_HND_RT_CONTINUE,
     MIBY_HND_RT_STOP,
-    NULL,			/* reserved */
+    NULL, /* reserved */
     MIBY_HND_RT_ACT_SENSE,
     MIBY_HND_RT_SYS_RESET
 };
@@ -134,192 +134,192 @@ static const rt_handler_t rt_handler_table[8] = {
 
 /*****************************************************************************/
 /**
-	Initialise an instance of the MIBY parser.
-	
-	@param this		Pointer to MIBY parser state object
-	@param v        User-supplied opaque pointer
+    Initialise an instance of the MIBY parser.
+
+    @param this     Pointer to MIBY parser state object
+    @param v        User-supplied opaque pointer
 **/
 void miby_init( miby_t *this, void *v )
 {
-	this->statusbyte = 0;
-	this->msg_chan   = 0;
-	this->sysexstate = MIBY_SYSEX_IDLE;
-	this->msglen     = 0;
-	this->ptr        = 0;
-	this->handler    = NULL;
-	this->v          = v;
-	
-	memset( this->buf, 0, sizeof( this->buf ) );
+    this->statusbyte = 0;
+    this->msg_chan   = 0;
+    this->sysexstate = MIBY_SYSEX_IDLE;
+    this->msglen     = 0;
+    this->ptr        = 0;
+    this->handler    = NULL;
+    this->v          = v;
+
+    memset( this->buf, 0, sizeof( this->buf ) );
 }
 
 /*****************************************************************************/
 /**
-	MIBY - MIDI bytestream parser
-	
-	@param this		Poinetr to MIBY parser state object
-	@param rxbyte	Byte to process
+    MIBY - MIDI bytestream parser
+
+    @param this     Poinetr to MIBY parser state object
+    @param rxbyte   Byte to process
 **/
 void miby_parse( miby_t *this, unsigned char rxbyte )
 {
-	int i;
-	
-	/* Catch System Realtime messages early - they can appear ANYWHERE in the 
-	 *  stream.
-	 */
-	if ( BYTE_IS_SYSRT( rxbyte ) )
-	{
-	    rt_handler_t h = rt_handler_table[rxbyte - STATUS_RT_TIMING];
-	    if ( h )
-        	(h)( this );
+    int i;
 
-	    return;
-	}
+    /* Catch System Realtime messages early - they can appear ANYWHERE in the
+     *  stream.
+     */
+    if ( BYTE_IS_SYSRT( rxbyte ) )
+    {
+        rt_handler_t h = rt_handler_table[rxbyte - STATUS_RT_TIMING];
+        if ( h )
+            (h)( this );
 
-	/* Treat data bytes and status bytes separately **************************/
-	if ( BYTE_IS_DATA( rxbyte ) )
-	{
-		/* If there is no status byte then this data byte is an orphan, or
-		 *  noise, so drop it.
-		 */
-	    if ( this->statusbyte == 0 )  
-        	return;
+        return;
+    }
 
-		/* Append the received byte to the receive buffer */
-	    this->buf[this->ptr++] = rxbyte;
+    /* Treat data bytes and status bytes separately **************************/
+    if ( BYTE_IS_DATA( rxbyte ) )
+    {
+        /* If there is no status byte then this data byte is an orphan, or
+         *  noise, so drop it.
+         */
+        if ( this->statusbyte == 0 )
+            return;
 
-		/* Until we have received the required number of data bytes we keep
-		 *  going.
-		 */
-	    if ( this->ptr < this->msglen )
-        	return;
+        /* Append the received byte to the receive buffer */
+        this->buf[this->ptr++] = rxbyte;
 
-		/* Ok, at this point we have received all the data bytes we are
-		 *  expecting.
-		 * First check if we are in the midst of a SysEx message.
-		 */
-	    if ( this->sysexstate != MIBY_SYSEX_IDLE )
-	    {	
-			/* This is part of a SysEx message.  Go and call the handler,
-			 *  which will set this->ptr to 0 to signal successful 
-			 *  consumption of the SysEx data (e.g. this SysEx is for this
-			 *  device).
-			 */
-	    	(this->handler)( this );
-        	if ( this->ptr )
-			{
-				/* This SysEx chunk caused a problem, so we move back to the
-				 *  IDLE state so that the rest of this SysEx message will be
-				 *  ignored.  Also clear out the status byte while we're at it.
-				 */
-        	    this->sysexstate = MIBY_SYSEX_IDLE;
-		    	this->statusbyte = 0;
-			}
-			else
-			{
-				/* The chunk was handled successfully so move (if not already)
-				 *  to the MIDdle state.
-				 */
-        	    this->sysexstate = MIBY_SYSEX_MID;
-			}
-	    }
-	    else /* Not in SysEx mode */
-	    {	
-			/* This is any other message, so just call its handler */
-		    (this->handler)( this );
+        /* Until we have received the required number of data bytes we keep
+         *  going.
+         */
+        if ( this->ptr < this->msglen )
+            return;
 
-			/* Channel messages can use Running Status.  If this is not a 
-			 *  Channel message then clear the status byte.
-			 */
-        	if ( !BYTE_IS_CHAN( this->statusbyte ) )
-        	    this->statusbyte = 0;
-	    }
+        /* Ok, at this point we have received all the data bytes we are
+         *  expecting.
+         * First check if we are in the midst of a SysEx message.
+         */
+        if ( this->sysexstate != MIBY_SYSEX_IDLE )
+        {
+            /* This is part of a SysEx message.  Go and call the handler,
+             *  which will set this->ptr to 0 to signal successful
+             *  consumption of the SysEx data (e.g. this SysEx is for this
+             *  device).
+             */
+            (this->handler)( this );
+            if ( this->ptr )
+            {
+                /* This SysEx chunk caused a problem, so we move back to the
+                 *  IDLE state so that the rest of this SysEx message will be
+                 *  ignored.  Also clear out the status byte while we're at it.
+                 */
+                this->sysexstate = MIBY_SYSEX_IDLE;
+                this->statusbyte = 0;
+            }
+            else
+            {
+                /* The chunk was handled successfully so move (if not already)
+                 *  to the MIDdle state.
+                 */
+                this->sysexstate = MIBY_SYSEX_MID;
+            }
+        }
+        else /* Not in SysEx mode */
+        {
+            /* This is any other message, so just call its handler */
+            (this->handler)( this );
 
-		/* Message processed, reset the receive buffer */
-	    this->ptr = 0;
-	}
-	else /* This is a status byte ********************************************/
-	{
-		/* Reset the status byte field ready for the new one */
-	    this->statusbyte = 0;
+            /* Channel messages can use Running Status.  If this is not a
+             *  Channel message then clear the status byte.
+             */
+            if ( !BYTE_IS_CHAN( this->statusbyte ) )
+                this->statusbyte = 0;
+        }
 
-		/* If we are in the middle of a SysEx message then we need to terminate
-		 *  it somehow.
-		 */
-	    if ( this->sysexstate != MIBY_SYSEX_IDLE )
-	    {
-			/* A status byte marks the end of a SysEx message */
-			this->sysexstate = MIBY_SYSEX_END;
-        	
-			/* However if the status byte is not an End Of Exclusive (EOX) 
-			 *  then we treat this as a aborted SysEx message.
-			 */
-        	if ( rxbyte != STATUS_EOX )
-        	    this->sysexstate = MIBY_SYSEX_ABORT; 
-				
-			/* Pass the received data to the SysEx handler */               
-        	(this->handler)( this );
-			
-			/* Then back to IDLE state ready for the start of the next one */
-			this->sysexstate = MIBY_SYSEX_IDLE;
-			
-			/* If this status byte is EOX then we consume it here.  Any other
-			 *  status byte needs to be further processed.
-			 */
-        	if ( rxbyte == STATUS_EOX )
-        	    return;
-	    }
+        /* Message processed, reset the receive buffer */
+        this->ptr = 0;
+    }
+    else /* This is a status byte ********************************************/
+    {
+        /* Reset the status byte field ready for the new one */
+        this->statusbyte = 0;
 
-		/* Channel status bytes have a channel field in then.  Extract it and
-		 *  then strip out the channel field to normalise the status bytes.
-		 */
-	    if ( BYTE_IS_CHAN( rxbyte ) )
-	    {
-        	unsigned char chan = rxbyte & MASK_CHAN;
+        /* If we are in the middle of a SysEx message then we need to terminate
+         *  it somehow.
+         */
+        if ( this->sysexstate != MIBY_SYSEX_IDLE )
+        {
+            /* A status byte marks the end of a SysEx message */
+            this->sysexstate = MIBY_SYSEX_END;
 
-			/* Quick hack: if the channel on this status byte is not active in
-			 *  the device then simply ignore this entire message.
-			 */
-        	if ( chan < this->basic_channel || chan > this->top_channel )
-        	    return;
-		    
-			rxbyte &= MASK_CMD;
-        	this->msg_chan = chan;
-	    }
+            /* However if the status byte is not an End Of Exclusive (EOX)
+             *  then we treat this as a aborted SysEx message.
+             */
+            if ( rxbyte != STATUS_EOX )
+                this->sysexstate = MIBY_SYSEX_ABORT;
 
-		/* Get the handler from the main message handler table.  We use a 
-		 *  hash to map all valid status bytes into a small table.
-		 */
-	    i = HASH(rxbyte);
-	    this->handler = msg_table[i].handler;
+            /* Pass the received data to the SysEx handler */
+            (this->handler)( this );
 
-		/* If there is no supplied handler for this status byte then drop the 
-		 *   message.
-		 */
-	    if ( this->handler == NULL )
-        	return;
-		
-		/* For a message with a handler get the required number of data bytes */
-	    this->msglen = msg_table[i].len;
+            /* Then back to IDLE state ready for the start of the next one */
+            this->sysexstate = MIBY_SYSEX_IDLE;
 
-		/* For messages with no data bytes we handle them here directly */
-	    if ( this->msglen == 0 )
-	    {
-        	(this->handler)( this );
-        	return;
-	    }
-		
-		/* Prepare the parser to process this message, setting up the
-		 *  statusbyte field and reseting the receive buffer.
-		 */
-	    this->statusbyte = rxbyte;
-	    this->ptr = 0;
+            /* If this status byte is EOX then we consume it here.  Any other
+             *  status byte needs to be further processed.
+             */
+            if ( rxbyte == STATUS_EOX )
+                return;
+        }
 
-		/* Finally, check if this is the start of a SysEx message */
-	    if ( this->statusbyte == STATUS_SYSEX )
-	    {
-        	this->sysexstate = MIBY_SYSEX_START;
-	    }        
-	}        
+        /* Channel status bytes have a channel field in then.  Extract it and
+         *  then strip out the channel field to normalise the status bytes.
+         */
+        if ( BYTE_IS_CHAN( rxbyte ) )
+        {
+            unsigned char chan = rxbyte & MASK_CHAN;
+
+            /* Quick hack: if the channel on this status byte is not active in
+             *  the device then simply ignore this entire message.
+             */
+            if ( chan < this->basic_channel || chan > this->top_channel )
+                return;
+
+            rxbyte &= MASK_CMD;
+            this->msg_chan = chan;
+        }
+
+        /* Get the handler from the main message handler table.  We use a
+         *  hash to map all valid status bytes into a small table.
+         */
+        i = HASH(rxbyte);
+        this->handler = msg_table[i].handler;
+
+        /* If there is no supplied handler for this status byte then drop the
+         *   message.
+         */
+        if ( this->handler == NULL )
+            return;
+
+        /* For a message with a handler get the required number of data bytes */
+        this->msglen = msg_table[i].len;
+
+        /* For messages with no data bytes we handle them here directly */
+        if ( this->msglen == 0 )
+        {
+            (this->handler)( this );
+            return;
+        }
+
+        /* Prepare the parser to process this message, setting up the
+         *  statusbyte field and reseting the receive buffer.
+         */
+        this->statusbyte = rxbyte;
+        this->ptr = 0;
+
+        /* Finally, check if this is the start of a SysEx message */
+        if ( this->statusbyte == STATUS_SYSEX )
+        {
+            this->sysexstate = MIBY_SYSEX_START;
+        }
+    }
 }
 
 /*****************************************************************************/
